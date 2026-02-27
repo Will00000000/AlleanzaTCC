7d9ac9ac37e34de2d77213ddf7a7533df7b1b803
Will00000000 <rm94807@estudante.fieb.edu.br>
2026-02-27 12:28:18 -0300

    V03 - W
    Projeteis e lixos se destroem

--- New Alleanza/Assets/Fase 1/Stepline/5 - MinigameLixos/Scripts/GameController.cs
+++ New Alleanza/Assets/Fase 1/Stepline/5 - MinigameLixos/Scripts/GameController.cs
@@ -5,13 +5,28 @@ public class GameController : MonoBehaviour
     public Transform [] pontoOrigem;
     public GameObject Lixo;
 
+    public float timer;
+    public float intervaloTempo;
+
     void Start ()
     {
-
+        timer = intervaloTempo;
     }
 
     void Update ()
     {
-        
+        criaAsteroides ();
+    }
+
+    void criaAsteroides ()
+    {
+        timer -= Time.deltaTime;
+
+        if (timer <= 0)
+        {
+            int pontoAleatorio = Random.Range (0, pontoOrigem.Length -1);
+            Instantiate (Lixo, pontoOrigem [pontoAleatorio].position, pontoOrigem [pontoAleatorio].rotation);
+            timer = intervaloTempo;
+        }
     }
 }
--- New Alleanza/Assets/Fase 1/Stepline/5 - MinigameLixos/Scripts/Tiros.cs
+++ New Alleanza/Assets/Fase 1/Stepline/5 - MinigameLixos/Scripts/Tiros.cs
@@ -5,6 +5,8 @@ public class Tiros : MonoBehaviour
     Rigidbody2D rig;
     [Range (0, 10)] public float velocidade;
 
+    public Transform lixo;
+
     void Start ()
     {
         rig = GetComponent <Rigidbody2D> ();
@@ -13,5 +15,13 @@ public class Tiros : MonoBehaviour
     void Update ()
     {
         transform.position = new Vector2 (transform.position.x + velocidade, transform.position.y);
+    }
+
+    void OnTriggerEnter2D (Collider2D col)
+    {
+        if (col.gameObject.tag == "lixo")
+        {
+            Destroy (gameObject);
+        }
     }
 }
