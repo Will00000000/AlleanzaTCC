1ae508369d3bde44a22d5c260fb151eeedcf350e
Will00000000 <rm94807@estudante.fieb.edu.br>
2026-03-02 12:30:08 -0300

    V05 - W
    Tentar deixar os lixos mais rápidos com o tempo

--- New Alleanza/Assets/Fase 1/Stepline/5 - MinigameLixos/Scripts/Lixos.cs
+++ New Alleanza/Assets/Fase 1/Stepline/5 - MinigameLixos/Scripts/Lixos.cs
@@ -7,6 +7,9 @@ public class Lixos : MonoBehaviour
     GameObject alvo;
     public int velocidade;
 
+    int lixosAcertados = 0;
+    bool lixoDestruido = false;
+
     void Start()
     {
         rig = GetComponent <Rigidbody2D> ();
@@ -16,6 +19,8 @@ public class Lixos : MonoBehaviour
     void Update()
     {
         transform.position = Vector2.MoveTowards (transform.position, alvo.transform.position, velocidade * Time.deltaTime);
+
+        //Pontuacao ();
     }
 
     void OnTriggerEnter2D (Collider2D col)
@@ -23,6 +28,16 @@ public class Lixos : MonoBehaviour
         if (col.gameObject.tag == "projetil")
         {
             Destroy(gameObject);
+            lixosAcertados += 1;
         }
     }
+
+    /*void Pontuacao ()
+    {
+        if (lixoDestruido = true)
+        {
+            lixosAcertados += 1;
+            Debug.Log (lixosAcertados);
+        }
+    }*/
 }
