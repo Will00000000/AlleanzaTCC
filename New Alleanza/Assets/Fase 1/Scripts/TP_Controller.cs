3676f6061e6af068e3dada55dcfebcd37526f8ea
Will00000000 <rm94807@estudante.fieb.edu.br>
2026-03-17 10:45:20 -0300

    V11 - W
    Sprites das ruas e introdução de teleportes

---
+++ New Alleanza/Assets/Fase 1/Scripts/TP_Controller.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+public class TP_Controller : MonoBehaviour
+{
+    public GameObject alvo;
+
+    public void TP_Rua ()
+    {
+        transform.position = new Vector2 (97.8300018f, 0);
+    }
+}
