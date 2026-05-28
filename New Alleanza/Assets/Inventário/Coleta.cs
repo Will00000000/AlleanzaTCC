5eec56e662349e21494cc7ae156c00dff58de9b8
Will00000000 <rm94807@estudante.fieb.edu.br>
2026-05-28 09:11:14 -0300

    V02.12 - W
    Botões brancos resolvidos, porém encontrei um problema na permanência de inventário entre cenas (tentei usar singleton, talvez seja o único método)

--- New Alleanza/Assets/Inventário/Coleta.cs
+++ New Alleanza/Assets/Inventário/Coleta.cs
@@ -15,7 +15,7 @@ public class Coleta : MonoBehaviour
         spriteItem = sprite_itensGeral[0].sprite; //pega o sprite 1 "chave"
     }
 
-    public void ColetarItem2()
+    public void ColetarPeca()
     {
         itensGeral[1].transform.localScale = new Vector3(0, 0, 0); //objeto dhave é desabilitado
 
--- New Alleanza/Assets/Inventário/VarreduraSlots.cs
+++ New Alleanza/Assets/Inventário/VarreduraSlots.cs
@@ -1,20 +1,25 @@
 using UnityEngine;
 using UnityEngine.UI;
 
-public class VarreduraSlots : MonoBehaviour
+public class VarreduraSlots : Singleton<VarreduraSlots>
 {
     public Image[] slot;
 
-    public Coleta script_coleta;
+    public GameObject script_coleta;
+
+    private void Start()
+    {
+        script_coleta = GameObject.Find("InteractController");
+    }
 
     private void Update()
     {
         Color novaCor;
 
-        #region
+        #region Slot 1
         if (slot[0].sprite == null)
         {
-            slot[0].sprite = script_coleta.spriteItem;
+            slot[0].sprite = script_coleta.GetComponent<Coleta>().spriteItem;
         }
         #endregion
 
@@ -25,11 +30,12 @@ public class VarreduraSlots : MonoBehaviour
             novaCor.a = 1f;
             slot[0].color = novaCor;
 
-            if (slot[0].sprite != script_coleta.spriteItem)
+            if (slot[0].sprite != script_coleta.GetComponent<Coleta>().spriteItem)
             {
-                slot[1].sprite = script_coleta.spriteItem;
+                slot[1].sprite = script_coleta.GetComponent<Coleta>().spriteItem;
             }
-            #endregion
+        }
+        #endregion
 
         #region Slot 3
         else if (slot[2].sprite == null)
@@ -38,11 +44,11 @@ public class VarreduraSlots : MonoBehaviour
             novaCor.a = 1f;
             slot[1].color = novaCor;
 
-            if (slot[0].sprite != script_coleta.spriteItem)
+            if (slot[0].sprite != script_coleta.GetComponent<Coleta>().spriteItem)
             {
-                if (slot[1].sprite != script_coleta.spriteItem)
+                if (slot[1].sprite != script_coleta.GetComponent<Coleta>().spriteItem)
                 {
-                    slot[2].sprite = script_coleta.spriteItem;
+                    slot[2].sprite = script_coleta.GetComponent<Coleta>().spriteItem;
                 }
             }
         }
@@ -50,93 +56,124 @@ public class VarreduraSlots : MonoBehaviour
 
         #region Slot 4
         else if (slot[3].sprite == null)
-            {
-                novaCor = slot[2].color;
-                novaCor.a = 1f;
-                slot[2].color = novaCor;
+        {
+            novaCor = slot[2].color;
+            novaCor.a = 1f;
+            slot[2].color = novaCor;
 
-                if (slot[0].sprite != script_coleta.spriteItem)
+            if (slot[0].sprite != script_coleta.GetComponent<Coleta>().spriteItem)
+            {
+                if (slot[1].sprite != script_coleta.GetComponent<Coleta>().spriteItem)
                 {
-                    if (slot[1].sprite != script_coleta.spriteItem)
+                    if (slot[2].sprite != script_coleta.GetComponent<Coleta>().spriteItem)
                     {
-                        if (slot[2].sprite != script_coleta.spriteItem)
+                        slot[3].sprite = script_coleta.GetComponent<Coleta>().spriteItem;
+                    }
+                }
+            }
+        }
+        #endregion
+
+        #region Slot 5
+        else if (slot[4].sprite == null)
+        {
+            novaCor = slot[3].color;
+            novaCor.a = 1f;
+            slot[3].color = novaCor;
+
+            if (slot[0].sprite != script_coleta.GetComponent<Coleta>().spriteItem)
+            {
+                if (slot[1].sprite != script_coleta.GetComponent<Coleta>().spriteItem)
+                {
+                    if (slot[2].sprite != script_coleta.GetComponent<Coleta>().spriteItem)
+                    {
+                        if (slot[3].sprite != script_coleta.GetComponent<Coleta>().spriteItem)
                         {
-                            slot[3].sprite = script_coleta.spriteItem;
+                            slot[4].sprite = script_coleta.GetComponent<Coleta>().spriteItem;
                         }
                     }
                 }
             }
-            #endregion
+        }
+        #endregion
 
-            #region Slot 5
-            else if (slot[4].sprite == null)
+        #region Slot 6
+        else if (slot[5].sprite == null)
+        {
+            novaCor = slot[4].color;
+            novaCor.a = 1f;
+            slot[4].color = novaCor;
+
+            if (slot[0].sprite != script_coleta.GetComponent<Coleta>().spriteItem)
             {
-                novaCor = slot[3].color;
-                novaCor.a = 1f;
-                slot[3].color = novaCor;
-
-                if (slot[0].sprite != script_coleta.spriteItem)
+                if (slot[1].sprite != script_coleta.GetComponent<Coleta>().spriteItem)
                 {
-                    if (slot[1].sprite != script_coleta.spriteItem)
+                    if (slot[2].sprite != script_coleta.GetComponent<Coleta>().spriteItem)
                     {
-                        if (slot[2].sprite != script_coleta.spriteItem)
+                        if (slot[3].sprite != script_coleta.GetComponent<Coleta>().spriteItem)
                         {
-                            if (slot[3].sprite != script_coleta.spriteItem)
+                            if (slot[4].sprite != script_coleta.GetComponent<Coleta>().spriteItem)
                             {
-                                slot[4].sprite = script_coleta.spriteItem;
+                                slot[5].sprite = script_coleta.GetComponent<Coleta>().spriteItem;
                             }
                         }
                     }
                 }
             }
-            #endregion
+        }
+        #endregion
 
-            #region Slot 6
-            else if (slot[5].sprite == null)
+        #region Slot 7
+        else if (slot[6].sprite == null)
+        {
+            novaCor = slot[5].color;
+            novaCor.a = 1f;
+            slot[5].color = novaCor;
+
+            if (slot[0].sprite != script_coleta.GetComponent<Coleta>().spriteItem)
             {
-                novaCor = slot[4].color;
-                novaCor.a = 1f;
-                slot[4].color = novaCor;
-
-                if (slot[0].sprite != script_coleta.spriteItem)
+                if (slot[1].sprite != script_coleta.GetComponent<Coleta>().spriteItem)
                 {
-                    if (slot[1].sprite != script_coleta.spriteItem)
+                    if (slot[2].sprite != script_coleta.GetComponent<Coleta>().spriteItem)
                     {
-                        if (slot[2].sprite != script_coleta.spriteItem)
+                        if (slot[3].sprite != script_coleta.GetComponent<Coleta>().spriteItem)
                         {
-                            if (slot[3].sprite != script_coleta.spriteItem)
+                            if (slot[4].sprite != script_coleta.GetComponent<Coleta>().spriteItem)
                             {
-                                if (slot[4].sprite != script_coleta.spriteItem)
+                                if (slot[5].sprite != script_coleta.GetComponent<Coleta>().spriteItem)
                                 {
-                                    slot[5].sprite = script_coleta.spriteItem;
+                                    slot[6].sprite = script_coleta.GetComponent<Coleta>().spriteItem;
                                 }
                             }
                         }
                     }
                 }
             }
-            #endregion
+        }
+        #endregion
 
-            #region Slot 7
-            else if (slot[6].sprite == null)
+        #region Slot 8
+        else if (slot[7].sprite == null)
+        {
+            novaCor = slot[6].color;
+            novaCor.a = 1f;
+            slot[6].color = novaCor;
+
+            if (slot[0].sprite != script_coleta.GetComponent<Coleta>().spriteItem)
             {
-                novaCor = slot[5].color;
-                novaCor.a = 1f;
-                slot[5].color = novaCor;
-
-                if (slot[0].sprite != script_coleta.spriteItem)
+                if (slot[1].sprite != script_coleta.GetComponent<Coleta>().spriteItem)
                 {
-                    if (slot[1].sprite != script_coleta.spriteItem)
+                    if (slot[2].sprite != script_coleta.GetComponent<Coleta>().spriteItem)
                     {
-                        if (slot[2].sprite != script_coleta.spriteItem)
+                        if (slot[3].sprite != script_coleta.GetComponent<Coleta>().spriteItem)
                         {
-                            if (slot[3].sprite != script_coleta.spriteItem)
+                            if (slot[4].sprite != script_coleta.GetComponent<Coleta>().spriteItem)
                             {
-                                if (slot[4].sprite != script_coleta.spriteItem)
+                                if (slot[5].sprite != script_coleta.GetComponent<Coleta>().spriteItem)
                                 {
-                                    if (slot[5].sprite != script_coleta.spriteItem)
+                                    if (slot[6].sprite != script_coleta.GetComponent<Coleta>().spriteItem)
                                     {
-                                        slot[6].sprite = script_coleta.spriteItem;
+                                        slot[7].sprite = script_coleta.GetComponent<Coleta>().spriteItem;
                                     }
                                 }
                             }
@@ -144,30 +181,33 @@ public class VarreduraSlots : MonoBehaviour
                     }
                 }
             }
-            #endregion
+        }
+        #endregion
 
-            #region Slot 8
-            else if (slot[7].sprite == null)
+        #region Slot 9
+        else if (slot[8].sprite == null)
+        {
+            novaCor = slot[7].color;
+            novaCor.a = 1f;
+            slot[7].color = novaCor;
+
+            if (slot[0].sprite != script_coleta.GetComponent<Coleta>().spriteItem)
             {
-                novaCor = slot[6].color;
-                novaCor.a = 1f;
-                slot[6].color = novaCor;
-
-                if (slot[0].sprite != script_coleta.spriteItem)
+                if (slot[1].sprite != script_coleta.GetComponent<Coleta>().spriteItem)
                 {
-                    if (slot[1].sprite != script_coleta.spriteItem)
+                    if (slot[2].sprite != script_coleta.GetComponent<Coleta>().spriteItem)
                     {
-                        if (slot[2].sprite != script_coleta.spriteItem)
+                        if (slot[3].sprite != script_coleta.GetComponent<Coleta>().spriteItem)
                         {
-                            if (slot[3].sprite != script_coleta.spriteItem)
+                            if (slot[4].sprite != script_coleta.GetComponent<Coleta>().spriteItem)
                             {
-                                if (slot[4].sprite != script_coleta.spriteItem)
+                                if (slot[5].sprite != script_coleta.GetComponent<Coleta>().spriteItem)
                                 {
-                                    if (slot[5].sprite != script_coleta.spriteItem)
+                                    if (slot[6].sprite != script_coleta.GetComponent<Coleta>().spriteItem)
                                     {
-                                        if (slot[6].sprite != script_coleta.spriteItem)
+                                        if (slot[7].sprite != script_coleta.GetComponent<Coleta>().spriteItem)
                                         {
-                                            slot[7].sprite = script_coleta.spriteItem;
+                                            slot[8].sprite = script_coleta.GetComponent<Coleta>().spriteItem;
                                         }
                                     }
                                 }
@@ -176,48 +216,14 @@ public class VarreduraSlots : MonoBehaviour
                     }
                 }
             }
-            #endregion
+        }
+        #endregion
 
-            #region Slot 9
-            else if (slot[8].sprite == null)
-            {
-                novaCor = slot[7].color;
-                novaCor.a = 1f;
-                slot[7].color = novaCor;
-
-                if (slot[0].sprite != script_coleta.spriteItem)
-                {
-                    if (slot[1].sprite != script_coleta.spriteItem)
-                    {
-                        if (slot[2].sprite != script_coleta.spriteItem)
-                        {
-                            if (slot[3].sprite != script_coleta.spriteItem)
-                            {
-                                if (slot[4].sprite != script_coleta.spriteItem)
-                                {
-                                    if (slot[5].sprite != script_coleta.spriteItem)
-                                    {
-                                        if (slot[6].sprite != script_coleta.spriteItem)
-                                        {
-                                            if (slot[7].sprite != script_coleta.spriteItem)
-                                            {
-                                                slot[8].sprite = script_coleta.spriteItem;
-                                            }
-                                        }
-                                    }
-                                }
-                            }
-                        }
-                    }
-                }
-            }
-            else
-            {
-                novaCor = slot[8].color;
-                novaCor.a = 1f;
-                slot[8].color = novaCor;
-            }
-            #endregion
+        else
+        {
+            novaCor = slot[8].color;
+            novaCor.a = 1f;
+            slot[8].color = novaCor;
         }
     }
 }
