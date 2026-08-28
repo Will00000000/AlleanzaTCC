901eeb2a7bd2c8944e15c6a97c753777a037951b
Will00000000 <rm94807@estudante.fieb.edu.br>
2026-08-28 09:11:32 -0300

    MinigamePoção #5
    Tentando programar a possibilidade do jogador jogar o item no caldeirão

--- New Alleanza/Assets/Fases/Fase 2/Jogo/Minigames/Poções/Scripts/SelecionarIngrediente.cs
+++ New Alleanza/Assets/Fases/Fase 2/Jogo/Minigames/Poções/Scripts/SelecionarIngrediente.cs
@@ -5,6 +5,8 @@ public class SelecionarIngrediente : MonoBehaviour
     public Transform[] ingredientes;
     public GameObject contornoDeSeleção;
 
+    bool ingredienteSelecionado; //se existe algum item selecionado no momento
+
     int índiceLista;
 
     private void Update()
@@ -17,7 +19,7 @@ public class SelecionarIngrediente : MonoBehaviour
     {
         contornoDeSeleção.transform.position = new Vector2(ingredientes[índiceLista].transform.position.x, ingredientes[índiceLista].transform.position.y); //o contorno de seleção segue o item em foco
 
-        if (Input.GetKeyDown(KeyCode.RightArrow)) //se o jogador apertar a seta para a direita...
+        if (ingredienteSelecionado == false && Input.GetKeyDown(KeyCode.RightArrow)) //se não houver nenhum item selecionado e o jogador apertar a seta para a direita...
         {
             índiceLista += 1; //... o foco vai para o próximo ingrediente da lista.
 
@@ -30,9 +32,18 @@ public class SelecionarIngrediente : MonoBehaviour
 
     void MoverIngrediente ()
     {
-        if (Input.GetKeyDown(KeyCode.Return))
+        if (Input.GetKeyDown(KeyCode.Return)) // se o jogador apertar Enter...
         {
-            ingredientes[índiceLista].GetComponent<MoverIngrediente>().enabled = true;
+            ingredientes[índiceLista].GetComponent<MoverIngrediente>().enabled = true; // ... o jogador consegue mover o ingrediente
+
+            ingredienteSelecionado = true; // ...agora é possível o jogar o ingrediente
+        }
+
+        if (Input.GetKeyDown(KeyCode.Return) && ingredienteSelecionado == true) // se o jogador apertar Enter e há um ingrediente selecionado
+        {
+            ingredientes[índiceLista].GetComponent<MoverIngrediente>().enabled = false; //... o jogador não consegue mais mover o ingrediente
+
+            ingredienteSelecionado = false; // ... não há mais nenhum ingrediente selecionada
         }
     }
 }
