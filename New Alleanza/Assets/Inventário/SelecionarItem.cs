3321641a9aeb053d4f8b8f9acffd56a92e4664f5
Will00000000 <rm94807@estudante.fieb.edu.br>
2026-09-15 10:42:33 -0300

    MinigameQuebraCabeça #4

---
+++ New Alleanza/Assets/Inventário/SelecionarItem.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SelecionarItem : MonoBehaviour
+{
+    Image spriteSlot;
+    private Color novaCor;
+
+    public Image espaçoItem;
+
+    private void Start()
+    {
+        spriteSlot = GetComponent<Image>();
+
+        novaCor.a = 0f;
+        espaçoItem.color = novaCor;
+    }
+
+    public void SelecaoItem ()
+    {
+        novaCor.a = 1f;
+        novaCor.r = 1f;
+        novaCor.g = 1f;
+        novaCor.b = 1f;
+
+        espaçoItem.sprite = spriteSlot.sprite;
+        espaçoItem.color = novaCor;
+    }
+}
